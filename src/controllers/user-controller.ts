5f0916d11805d4dcae595ac83a42c571baae437b
Arnaldo Ucuassapi <arnaldoucuassapi@gmail.com>
2023-10-06 01:22:20 +0100

    update user controller

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,19 +1,52 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { IController } from "../utils/@types";
+import { prisma } from "../utils/prisma";
+import { z } from "zod";
 
 export class UserController implements IController {
-  create(request: FastifyRequest, reply: FastifyReply) {
+  async create(request: FastifyRequest, reply: FastifyReply) {
+    const bodySchema = z.object({
+      avatar: z.string().or(z.undefined()),
+      name: z.string(),
+      email: z.string().email(),
+      password: z.string().min(8, "The password has ")
+    });
 
+    const { avatar, name, email, password } = bodySchema.parse(request.body);
+
+    await prisma.user.create({
+      data: {
+        avatar,
+        name,
+        email,
+        password
+      }
+    });
+
+    return reply.status(201).send();
   };
 
-  list(request: FastifyRequest, reply: FastifyReply) {
-    console.log("users");
+  async list(request: FastifyRequest, reply: FastifyReply) {
+    const users = await prisma.user.findMany();
 
-    return [{
-      name: "Arnaldo",
-      age: 14
-    }]
+    return users;
   };
+
+  async get(request: FastifyRequest) {
+    const paramsSchema = z.object({
+      id: z.string().uuid()
+    });
+
+    const { id } = paramsSchema.parse(request.params);
+
+    const user = await prisma.user.findUniqueOrThrow({
+      where: {
+        id
+      }
+    });
+
+    return user;
+  }
 
   update(request: FastifyRequest, reply: FastifyReply) {
 
