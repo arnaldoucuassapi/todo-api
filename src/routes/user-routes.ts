e6cb6b41732e94bb69bf564ca33e73e9d8f734e9
Arnaldo Ucuassapi <arnaldoucuassapi@gmail.com>
2023-10-06 01:22:57 +0100

    update user routes

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -2,11 +2,11 @@ import { FastifyInstance } from "fastify";
 import { UserController } from "../controllers/user-controller";
 
 export async function userRoutes(fastify: FastifyInstance) {
-
   const userController = new UserController();
   
-  fastify.post('/user', userController.create);
+  fastify.post('/users', userController.create);
   fastify.get('/users', userController.list);
-  fastify.put('/user', userController.update);
-  fastify.delete('/user', userController.delete);
+  fastify.get('/users/:id', userController.get);
+  fastify.put('/users', userController.update);
+  fastify.delete('/users', userController.delete);
 }
