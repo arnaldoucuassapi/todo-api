84a87d62f79fc34a8f0d9ae02a6d5834699f2ea8
Arnaldo Ucuassapi <arnaldoucuassapi@gmail.com>
2023-10-11 09:54:38 +0100

    update generate-token function

--- src/utils/generate-token.ts
+++ src/utils/generate-token.ts
@@ -1,13 +1,20 @@
 import 'dotenv/config';
 import jwt from "jsonwebtoken";
+import { z } from 'zod';
 
 type PayloadType = { 
   id: string 
 };
 
 export function generateToken(payload: PayloadType) {
+  const payloadSchema = z.object({
+    id: z.string().uuid("need to be uuid")
+  });
+
+  payload = payloadSchema.parse(payload);
+
   const secretKey = process.env.JWT_SECRET_KEY as string;
-  const expiresIn = Math.floor((Date.now() / 1000));
+  const expiresIn = '30s';
 
   const token = jwt.sign(payload, secretKey, {
     expiresIn
