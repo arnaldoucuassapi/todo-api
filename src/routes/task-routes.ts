36a7826c26674330c33b61ea953d7aaa50af31f9
Arnaldo Ucuassapi <arnaldoucuassapi@gmail.com>
2023-10-21 22:15:53 +0100

    update routes list, create; adding auth middleware

--- src/routes/task-routes.ts
+++ src/routes/task-routes.ts
@@ -1,11 +1,18 @@
 import { FastifyInstance } from "fastify";
 import { TaskController } from '../controllers/task-controller';
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 export async function taskRoutes(fastify: FastifyInstance) {
   const taskController = new TaskController();
   
-  fastify.post('/create', taskController.create);
-  fastify.get('/', taskController.list);
+  fastify.post('/create', {
+    preHandler: authMiddleware
+  }, taskController.create);
+  
+  fastify.get('/', {
+    preHandler: authMiddleware
+  }, taskController.list);
+  
   fastify.get('/?search', taskController.search);
   fastify.put('/update/:id', taskController.update);
   fastify.delete('/delete/:id', taskController.delete);
