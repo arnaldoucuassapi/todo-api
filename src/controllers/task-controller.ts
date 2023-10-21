872836492a6947d3228cda3547eac604f431ebe7
Arnaldo Ucuassapi <arnaldoucuassapi@gmail.com>
2023-10-21 22:17:51 +0100

    update create and list handlers

--- src/controllers/task-controller.ts
+++ src/controllers/task-controller.ts
@@ -1,29 +1,57 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { IController } from "../types";
 import { prisma } from "../utils/prisma";
+import { z } from "zod";
 
 export class TaskController implements IController {
   async create(request: FastifyRequest, reply: FastifyReply) {
+    const { id: userId } = request.context.tokenValues;
+    
+    const bodySchema = z.object({
+      title: z.string(),
+      description: z.string().or(z.undefined())
+    });
 
+    const { title, description } = bodySchema.parse(request.body);
+
+    await prisma.task.create({
+      data: {
+        title,
+        description,
+        User: {
+          connect: {
+            id: userId
+          }
+        }
+      },
+    });
+
+    return reply.status(201).send();
   };
 
   async list(request: FastifyRequest, reply: FastifyReply) {
+    const { id: userId } = request.context.tokenValues;
     
+    const tasks = await prisma.task.findMany({
+      where: {
+        userId
+      },
+      select: {
+        id: true,
+        title: true,
+        description: true,
+        done: true
+      }
+    });
+    
+    return tasks;
   };
 
-  async search() {
+  async search() {}
 
-  }
+  async update(request: FastifyRequest, reply: FastifyReply) {};
 
-  async update(request: FastifyRequest, reply: FastifyReply) {
+  async delete(request: FastifyRequest, reply: FastifyReply) {};
 
-  };
-
-  async delete(request: FastifyRequest, reply: FastifyReply) {
-
-  };
-
-  async done() {
-
-  }
+  async done() {}
 }
